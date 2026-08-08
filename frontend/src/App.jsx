5284853fecaf4bba902bf54e23f22221620573f3
afiaafia <afiaanna68@gmail.com>
2026-08-08 12:52:14 +0600

    Build and style Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
-import Hero from './components/Hero';
+import { Routes, Route } from 'react-router-dom';
+
+import MainLayout from './layouts/MainLayout';
+
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import SpaceDetails from './pages/SpaceDetails';
+
 import './App.css';
 
 function App() {
   return (
-    <>
-      <Hero />
-    </>
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/spaces/:id" element={<SpaceDetails />} />
+      </Route>
+    </Routes>
   );
 }
 
