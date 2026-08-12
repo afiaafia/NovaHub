7df25fff2ab46d72dac03ab415c2aee38b99ea18
afiaafia <afiaanna68@gmail.com>
2026-08-12 16:18:30 +0600

    changed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,27 +1,54 @@
 import Hero from '../components/Hero';
 import TrustedBy from '../components/TrustedBy';
 import FeaturesPreview from '../components/FeaturesPreview';
+import HowItWorks from '../components/HowItWorks';
+import Pricing from '../components/Pricing';
+import About from '../components/About';
+import Blog from '../components/Blog';
+import Contact from '../components/Contact';
 
 const Home = () => {
   return (
     <div className="home-page">
       {/* =================================
-          HERO SECTION
+          HERO
       ================================== */}
-
       <Hero />
 
       {/* =================================
           TRUSTED BY
       ================================== */}
-
       <TrustedBy />
 
       {/* =================================
           FEATURES PREVIEW
       ================================== */}
-
       <FeaturesPreview />
+
+      {/* =================================
+          HOW IT WORKS
+      ================================== */}
+      <HowItWorks />
+
+      {/* =================================
+          PRICING
+      ================================== */}
+      <Pricing />
+
+      {/* =================================
+          ABOUT
+      ================================== */}
+      <About />
+
+      {/* =================================
+          BLOG
+      ================================== */}
+      <Blog />
+
+      {/* =================================
+          CONTACT
+      ================================== */}
+      <Contact />
     </div>
   );
 };
